85970be6d61020384bed27cb418e8004d80fb63f
Nafanya <konstantin.konahevich@uacoders.com>
2019-12-27 06:30:14 +0200

    database config change

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-DATABASE_URL="mysql://b0020f7309241e:e86cbfa9@eu-cdbr-west-02.cleardb.net/heroku_7da8c0f740d2187?reconnect=true"

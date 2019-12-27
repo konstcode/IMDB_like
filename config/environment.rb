af73d50905fb3fd5c457570a1bba16efe208aff1
Nafanya <konstantin.konahevich@uacoders.com>
2019-12-27 05:39:44 +0200

    production database config fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+DATABASE_URL="mysql://urr9vfe6hrs5h1fp:xlf0ekx9aodwjh98@n7qmaptgs6baip9z.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/rt85poa2ivo952zq"

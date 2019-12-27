48bab2b1b27ed84ae602732cef7a6519284b2878
Nafanya <konstantin.konahevich@uacoders.com>
2019-12-27 06:07:43 +0200

    production database config change

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-DATABASE_URL="mysql://urr9vfe6hrs5h1fp:xlf0ekx9aodwjh98@n7qmaptgs6baip9z.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/rt85poa2ivo952zq"
+DATABASE_URL="mysql://b0020f7309241e:e86cbfa9@eu-cdbr-west-02.cleardb.net/heroku_7da8c0f740d2187?reconnect=true"
